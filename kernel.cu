8bef10d1c76c973f31d8650494d44bfd5d3de0bf
David Kouřil <david.kouril@gmail.com>
2015-11-15 11:52:55 +0100

    thread indexing

--- kernel.cu
+++ kernel.cu
@@ -8,11 +8,14 @@
  */
 __global__ void solveIteration(int **cells, int *cellsOut, int n) {
   
-  int i = threadIdx.x;
-  int j = threadIdx.y;
-  int k = threadIdx.z;
+  //int i = threadIdx.x;
+  //int j = threadIdx.y;
+  //int k = threadIdx.z;
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  int k = blockIdx.z * blockDim.z + threadIdx.z;
 
-  int alive = 0;
+  int alive = 0; // number of alive neighbours
   for (int ii = max(i-1, 0); ii <= min(i+1, n-1); ii++)
     for (int jj = max(j-1, 0); jj <= min(j+1, n-1); jj++)
       for (int kk = max(k-1, 0); kk <= min(k+1, n-1); kk++)
@@ -38,18 +41,21 @@ void solveGPU(int **dCells, int n, int iters){
   int *cellsNextIter = NULL;
   cudaMalloc((void**)&cellsNextIter, n*n*n*sizeof(cellsNextIter[0])); // TODO error checking
   
-  for (int i = 0; i < n; n++) {
+  for (int i = 0; i < iters; i++) {
     // grid and block dimensions setup
-    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid();
+    dim3 dimBlock(n, n, n);
+    //dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid(n, n);
     // kernel invocation
-    solveIteration<<<n, dimBlock>>>(dCells, cellsNextIter, n);
+    solveIteration<<<1, dimBlock>>>(dCells, cellsNextIter, n);
 
     // TODO swap grids
     int *tmp = *dCells;
     *dCells = cellsNextIter;
     cellsNextIter = tmp; // unnecessary
   }
+
+  // TODO free allocated memory
 	
 }
 
