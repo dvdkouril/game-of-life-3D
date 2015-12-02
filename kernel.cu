4ea2fbfb1588433c42f69283e519276300c5d6c2
David Kouřil <david.kouril@gmail.com>
2015-12-02 23:24:18 +0100

    FUCK YEAH! It works at ~2000 megacells/s

--- kernel.cu
+++ kernel.cu
@@ -1,8 +1,7 @@
 // write your code into this file
-//#define BLOCK_SIZE 8
-#define BLOCK_SIZE_X 8
+#define BLOCK_SIZE_X 16
 #define BLOCK_SIZE_Y 8
-#define BLOCK_SIZE_Z 8
+#define BLOCK_SIZE_Z 4
 //#define BLOCK_SIZE 8 // debug
 
 /*
@@ -17,57 +16,97 @@ __device__ int linearize(int x, int y, int z, int n_x, int n_y, int n_z) {
 }
 
 /*
-  int ** cells         input cells grid
-  int *  cellsOut      ouput cells grid - result of one iteration
-  int    n             grid x/y/z dimension
+  This should be actually named reduceHorizontally but I don't have enough time to change it ATM
  */
-__global__ void solveIteration(int *cells, int *cellsOut, int n) {
-  
-  int i = blockIdx.x * (blockDim.x-2) + threadIdx.x - 1; // global x coord (IN MEMORY, NOT BLOCK!!!)
-  int j = blockIdx.y * (blockDim.y-2) + threadIdx.y - 1; // global y coord
-  int k = blockIdx.z * (blockDim.z-2) + threadIdx.z - 1; // global z coord
+__global__ void reduceVertically(int *input, int *vertReduced, int n) {
+  int i = blockIdx.x * (blockDim.x) + threadIdx.x; // global x coord
+  int j = blockIdx.y * (blockDim.y) + threadIdx.y; // global y coord
+  int k = blockIdx.z * (blockDim.z) + threadIdx.z; // global z coord
 
-  int tx = threadIdx.x;  // block-local x coord
-  int ty = threadIdx.y;  // block-local y coord
-  int tz = threadIdx.z;  // block-local z coord
+  int cellId = linearize(i, j, k, n);
+  int cellRightId = linearize(i + 1, j, k, n);
+  int cellLeftId = linearize(i - 1, j, k, n);
+
+  int cellLeft = (i - 1 < 0) ? 0 : input[cellLeftId];
+  int cellRight = (i + 1 > n - 1) ? 0 : input[cellRightId];
+  //if ((i - 1 >= 0) && (i + 1 <= n - 1)) {
+    int sum = input[cellId] + cellLeft + cellRight;
+    if (input[cellId] == 1) sum *= -1;
+    vertReduced[cellId] = sum;
+    //}
+}
+
+__global__ void reduceHorizontally(int *vertReduced, int *horReduced, int n) {
+  int i = blockIdx.x * (blockDim.x) + threadIdx.x; // global x coord
+  int j = blockIdx.y * (blockDim.y) + threadIdx.y; // global y coord
+  int k = blockIdx.z * (blockDim.z) + threadIdx.z; // global z coord
+
+  int cellId = linearize(i, j, k, n);
+  int cellUpId = linearize(i, j - 1, k, n);
+  int cellDownId = linearize(i, j + 1, k, n);
+
+  int cellUp = (j - 1 < 0) ? 0 : vertReduced[cellUpId];
+  int cellDown = (j + 1 > n - 1) ? 0 : vertReduced[cellDownId];
+  int cell = vertReduced[cellId];
+  //if ((j - 1 >= 0) && (j + 1) <= n - 1) {
+  int sum = abs(cell) + abs(cellUp) + abs(cellDown);
+  if (cell < 0) {
+    sum *= -1;
+  }
+    //if (vertReduced[cellId] < 0) // if current cell is negative
+      //sum -= 1; // don't add current cell
+    horReduced[cellId] = sum;
+    //}
+}
+
+__global__ void reduceZically(int *horReduced, int *neighbours, int n) {
+  int i = blockIdx.x * (blockDim.x) + threadIdx.x; // global x coord
+  int j = blockIdx.y * (blockDim.y) + threadIdx.y; // global y coord
+  int k = blockIdx.z * (blockDim.z) + threadIdx.z; // global z coord
+
+  int cellId = linearize(i, j, k, n);
+  int cellForthId = linearize(i, j, k + 1, n);
+  int cellBackId = linearize(i, j, k - 1, n);
+
+  int cellBack = (k - 1 < 0) ? 0 : horReduced[cellBackId];
+  int cellForth = (k + 1 > n - 1) ? 0 : horReduced[cellForthId];
+  int cell = horReduced[cellId];
+  int sum = abs(cell) + abs(cellBack) + abs(cellForth);
+  if (cell < 0) { // if current cell is negative
+    sum *= -1;
+    sum += 1; // don't add current cell, this is still negative (sign that there was 1)
+  }
+  neighbours[cellId] = sum;
+    //}
+}
+
+
+/*
+  int *  neighbours         grid with neighbours computed for each cell
+  int *  cellsOut           ouput cells grid - result of one iteration
+  int    n                  grid x/y/z dimension
+ */
+__global__ void solveIteration(int *neighbours, int *cellsOut, int n) {
+  
+  int i = blockIdx.x * (blockDim.x) + threadIdx.x; // global x coord (IN MEMORY, NOT BLOCK!!!)
+  int j = blockIdx.y * (blockDim.y) + threadIdx.y; // global y coord
+  int k = blockIdx.z * (blockDim.z) + threadIdx.z; // global z coord
   
   int cellId = linearize(i, j, k, n);
-  
-  // alocating memory with 1 cell border
-  __shared__ int cellsBlock[BLOCK_SIZE_X * BLOCK_SIZE_Y * BLOCK_SIZE_Z];
+  int alive = neighbours[cellId];
+  int aliveAbs = abs(alive);
 
-  // TODO copy stuff from global memory to shared memory
-  if ((i < 0) || (j < 0) || (k < 0) || (i > n - 1) || (j > n - 1) || (k > n - 1)) {
-    cellsBlock[linearize(tx, ty, tz, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)] = 0;
+  if (aliveAbs < 4 || aliveAbs > 5) {
+    cellsOut[cellId] = 0;
+  } else if (aliveAbs == 5) {
+    cellsOut[cellId] = 1;
   } else {
-    cellsBlock[linearize(tx, ty, tz, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)] = cells[cellId];
-  }
-  
-  __syncthreads();
-  
-  // TODO use stuff from shared memory when computing alive neighbours
-  if ((i >= 0) && (j >= 0) && (k >= 0)&& (i <= n - 1) && (j <= n - 1) && (k <= n - 1)) {
-    if ((tx > 0) && (tx < BLOCK_SIZE_X - 1) &&
-	(ty > 0) && (ty < BLOCK_SIZE_Y - 1) &&
-	(tz > 0) && (tz < BLOCK_SIZE_Z - 1)) {
-      int alive = 0;
-      for (int ii = max(tx - 1, 0); ii <= min(tx + 1, BLOCK_SIZE_X - 1); ii++)
-	for (int jj = max(ty - 1, 0); jj <= min(ty + 1, BLOCK_SIZE_Y - 1); jj++) 
-	  for (int kk = max(tz - 1, 0); kk <= min(tz + 1, BLOCK_SIZE_Z - 1); kk++)
-	    alive += cellsBlock[linearize(ii, jj, kk, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)];
-      alive -= cellsBlock[linearize(tx, ty, tz, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)];
-	
-      if (alive < 4 || alive > 5) {
-	cellsOut[cellId] = 0;
-      } else if (alive == 5) {
-	cellsOut[cellId] = 1;
-      } else {
-	cellsOut[cellId] = cellsBlock[linearize(tx, ty, tz, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)];
-      }
-
-    }
-  }
-  
+    if (alive < 0)
+      cellsOut[cellId] = 1;
+    else
+      cellsOut[cellId] = 0;
+    //cellsOut[cellId] = [linearize(tx, ty, tz, BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z)];
+  }  
   
 }
 
@@ -93,26 +132,31 @@ void printGrid(int *cells, int n) {
  */
 void solveGPU(int **dCells, int n, int iters){
   // alocate array for computing next iteration
-  int *cellsNextIter = NULL;
+  int *cellsTemp = NULL;
   // memory allocated on the graphics card (can't be accessed in this function!!!)
-  if (cudaMalloc((void**)&cellsNextIter, n*n*n*sizeof(cellsNextIter[0])) != cudaSuccess) {
+  if (cudaMalloc((void**)&cellsTemp, n*n*n*sizeof(cellsTemp[0])) != cudaSuccess) {
     printf("Device memory allocation error\n");
   }
   
   for (int i = 0; i < iters; i++) {
     // grid and block dimensions setup
     dim3 dimBlock(BLOCK_SIZE_X, BLOCK_SIZE_Y, BLOCK_SIZE_Z);
-    int blocksNumX = (int)ceil(n / (float)(BLOCK_SIZE_X - 2));
-    int blocksNumY = (int)ceil(n / (float)(BLOCK_SIZE_Y - 2));
-    int blocksNumZ = (int)ceil(n / (float)(BLOCK_SIZE_Z - 2));
+    int blocksNumX = (int)ceil(n / (BLOCK_SIZE_X));
+    int blocksNumY = (int)ceil(n / (BLOCK_SIZE_Y));
+    int blocksNumZ = (int)ceil(n / (BLOCK_SIZE_Z));
     dim3 dimGrid(blocksNumX, blocksNumY, blocksNumZ);
 
-    // debug
-    //printf("grid size: %dx%dx%d\n", blocksNum, blocksNum, blocksNum);
-    //printf("block size: %dx%dx%d\n", BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    // TODO call kernel that reduces grid horizontally
+    reduceVertically<<<dimGrid, dimBlock>>>(*dCells, cellsTemp, n);
+    // TODO call kernel that reduces reduced grid vertically
+    reduceHorizontally<<<dimGrid, dimBlock>>>(cellsTemp, *dCells, n);
+
+    reduceZically<<<dimGrid, dimBlock>>>(*dCells, cellsTemp, n);
     
     // kernel invocation
-    solveIteration<<<dimGrid, dimBlock>>>(*dCells, cellsNextIter, n);
+    // cellsTemp = grid with neighbours computed for each cell
+    // dCells = grid for storing result
+    solveIteration<<<dimGrid, dimBlock>>>(cellsTemp, *dCells, n);
 
     // kernel invocation error checking
     cudaError_t cudaerr = cudaDeviceSynchronize();
@@ -120,10 +164,11 @@ void solveGPU(int **dCells, int n, int iters){
         printf("kernel launch failed with error \"%s\".\n",
                cudaGetErrorString(cudaerr));
 
+    // TODOOOOOOOOOOOOO I don't think I need to do this - think about it later!!!!
     // swap grids
-    int *tmp = *dCells;
-    *dCells = cellsNextIter; // setting newly computed iteration to as the result
-    cellsNextIter = tmp; // unnecessary
+    //int *tmp = *dCells;
+    //*dCells = cellsNextIter; // setting newly computed iteration to as the result
+    //cellsNextIter = tmp; // unnecessary
 
   }
   	
